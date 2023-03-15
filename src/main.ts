698956b9ff4da909bf4b6889f511b9db79aa298a
HTMLhead <mrdoo309@gmail.com>
2023-03-15 23:56:48 +0900

    feat(main): 요구사항의 동작이 올바르게 동작하도록 기능 제작

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
-import { getIssueTpl, getLabelItemTpl, getLabelTpl } from "./tpl";
-import { $ } from "./utils";
+import { getIssueInfoList } from "./apis/getIssueInfoList";
+import { checkIssueState } from "./helper/issue";
+import { renderIssueList } from "./render";
+import { getIssueTpl } from "./tpl";
+import { $, pipe } from "./utils";
 
 async function init() {
+  const issueInfoList = await getIssueInfoList();
+
+  const openIssue = issueInfoList.filter(checkIssueState("open"));
+  const closedIssue = issueInfoList.filter(checkIssueState("close"));
+
   const app = $("#app");
-  if (app) app.innerHTML = getIssueTpl();
+  if (app) app.innerHTML = getIssueTpl({ open: openIssue.length, closed: closedIssue.length });
+  const issueListContainer = $(".issue-list > ul") as Element;
+
+  await renderIssueList(issueInfoList, "open", issueListContainer);
+  await attachEvent(issueListContainer);
+}
+
+async function attachEvent(ele: Element) {
+  const openBtn = $(".open-count");
+  const closeBtn = $(".close-count");
+
+  openBtn?.addEventListener("click", async () => {
+    const issueInfoList = await getIssueInfoList();
+    openBtn.classList.add("font-bold");
+    closeBtn?.classList.remove("font-bold");
+    await renderIssueList(issueInfoList, "open", ele);
+  });
+  closeBtn?.addEventListener("click", async () => {
+    const issueInfoList = await getIssueInfoList();
+    closeBtn.classList.add("font-bold");
+    openBtn?.classList.remove("font-bold");
+    await renderIssueList(issueInfoList, "close", ele);
+  });
 }
 
 init();
