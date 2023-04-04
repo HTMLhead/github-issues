e1c7b5b58a50ed9f44d787c6d6ceb31d0595682c
HTMLhead <mrdoo309@gmail.com>
2023-04-05 00:35:06 +0900

    feat(events): 각 페이지별로 이벤트를 등록하는 폴더 제작 및 적용;

---
+++ src/events/issue/addIssueStatusBtnEvent.ts
@@ -0,0 +1,18 @@
+import { getIssueInfoList } from "@/apis/getIssueInfoList";
+import { renderIssueList } from "@/render";
+
+const addIssueStatusBtnEvent = (
+  targetEle: Element,
+  otherEle: Element,
+  containerEle: Element,
+  status: string
+) => {
+  targetEle?.addEventListener("click", async () => {
+    const issueInfoList = await getIssueInfoList();
+    targetEle.classList.add("font-bold");
+    otherEle.classList.remove("font-bold");
+    await renderIssueList(issueInfoList, status, containerEle);
+  });
+};
+
+export default addIssueStatusBtnEvent;
---
+++ src/events/issue/attachEvent.ts
@@ -0,0 +1,12 @@
+import { addIssueStatusBtnEvent } from "../../helper/attachEvent";
+import { $ } from "../../utils";
+
+async function attachEvent(containerEle: Element) {
+  const openBtn = $(".open-count") as Element;
+  const closeBtn = $(".close-count") as Element;
+
+  addIssueStatusBtnEvent(openBtn, closeBtn, containerEle, "open");
+  addIssueStatusBtnEvent(closeBtn, openBtn, containerEle, "close");
+}
+
+export default attachEvent
