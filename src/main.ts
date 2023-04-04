802754d35525c2552fb5c6582a465a6535b9136a
HTMLhead <mrdoo309@gmail.com>
2023-04-05 00:34:55 +0900

    update(main): 각 페이지 렌더링 방법을 참조해 init함수 추가

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,11 @@
-import { getIssueInfoList } from "./apis/getIssueInfoList";
-import { renderIssueList } from "./render";
-// tpl
-import { getIssueTpl } from "./tpl";
-// helper
-import { checkIssueState } from "./helper/issue";
-import { addIssueStatusBtnEvent } from "./helper/attachEvent";
-// utils
-import { $ } from "./utils";
+// render
+import { renderIssuePage } from "@/render";
+// events
+import { attachEvent } from "@/events/main";
 
 async function init() {
-  const issueInfoList = await getIssueInfoList();
-
-  const openIssue = issueInfoList.filter(checkIssueState("open"));
-  const closedIssue = issueInfoList.filter(checkIssueState("close"));
-
-  const app = $("#app");
-  if (app) app.innerHTML = getIssueTpl({ open: openIssue.length, closed: closedIssue.length });
-  const issueListContainer = $(".issue-list > ul") as Element;
-
-  await renderIssueList(issueInfoList, "open", issueListContainer);
-  await attachEvent(issueListContainer);
-}
-
-async function attachEvent(containerEle: Element) {
-  const openBtn = $(".open-count") as Element;
-  const closeBtn = $(".close-count") as Element;
-
-  addIssueStatusBtnEvent(openBtn, closeBtn, containerEle, "open");
-  addIssueStatusBtnEvent(closeBtn, openBtn, containerEle, "close");
+  attachEvent();
+  renderIssuePage();
 }
 
 init();
