02ab12ea402fdd2af633849262bf92b2cfb8d563
HTMLhead <mrdoo309@gmail.com>
2023-04-06 02:58:40 +0900

    remove(helper/attachEvent, renderLabelList): 사용하지 않는 render파일, attachEvent helper제거

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,4 +1,3 @@
 export { default as renderIssueList } from "./renderIssueList";
 export { default as renderIssuePage } from "./renderIssuePage";
 export { default as renderLabelPage } from "./renderLabelPage";
-export { default as renderLabelList } from "./renderLabelList";
