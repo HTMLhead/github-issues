31320e455e679400bddb3dbe2c88a5d7484dfa1c
HTMLhead <mrdoo309@gmail.com>
2023-04-06 02:57:39 +0900

    update(main): proxy함수를 통해 렌더링하도록 수정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-// render
-import { renderIssuePage } from "@/render";
+import { proxy } from "@/store";
+// apis
+import { getIssueInfoList } from "@/apis/getIssueInfoList";
+import { getLabelInfoList } from "@/apis/getLabelInfoList";
 // events
 import { attachEvent } from "@/events/main";
 
 async function init() {
+  const issueInfoList = await getIssueInfoList();
+  const labelInfoList = await getLabelInfoList();
+
+  proxy.issueList = issueInfoList;
+  proxy.labelList = labelInfoList;
+
   attachEvent();
-  renderIssuePage();
 }
 
 init();
